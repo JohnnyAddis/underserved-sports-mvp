716db4b5c484ceb942e52849b82e9ce93aa43fb0
Johnny Addis <johnnyaddis23@gmail.com>
2025-08-11 13:18:51 -0500

    ENV vars and fixing sanity config

--- sports-mvp/src/lib/sanity.ts
+++ sports-mvp/src/lib/sanity.ts
@@ -1,9 +1,18 @@
 import { createClient } from '@sanity/client'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const apiVersion = process.env.SANITY_API_VERSION || '2025-01-01'
+
+if (!projectId) {
+  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID. Did you set it in .env.local or Vercel?')
+}
+
 export const sanity = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  apiVersion: process.env.SANITY_API_VERSION || '2025-01-01',
+  projectId,
+  dataset,
+  apiVersion,
   useCdn: true,
-  // token: process.env.SANITY_READ_TOKEN, // only if data set is private...
+  // token: process.env.SANITY_READ_TOKEN, // only if dataset is private
+  perspective: 'published',
 })
